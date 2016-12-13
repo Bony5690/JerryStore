0232b22a688b40899db370b89b21dfe1d706d227
jerry bony <jerrybony@yahoo.com>
2016-12-13 12:08:31 -0600

    Example of creating a new routing

--- JerryStore/App_Start/RouteConfig.cs
+++ JerryStore/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace JerryStore
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "students",
+                url: "students/{id}",
+                defaults: new { controller = "students", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
