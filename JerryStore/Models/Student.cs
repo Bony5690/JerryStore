43775c391c7a93519520b6e2cf8737e55f246860
jerry bony <jerrybony@yahoo.com>
2016-12-14 12:22:08 -0600

    Updated student view

--- JerryStore/Models/Student.cs
+++ JerryStore/Models/Student.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System;
 
 namespace JerryStore.Models
 {
     public class Student
     {
+        [Display(Name= "ID")]
         public int ID { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Birth Of Date")]
         public DateTime? DateOfBirth { get; set; }
     }
 }
