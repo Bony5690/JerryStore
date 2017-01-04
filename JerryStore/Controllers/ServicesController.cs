51043079b7dfb1178a7b3ee07726b5675a94ea5d
Jerry bony <jerrybony@yahoo.com>
2017-01-04 11:44:48 -0600

    Updated store

--- JerryStore/Controllers/ServicesController.cs
+++ JerryStore/Controllers/ServicesController.cs
@@ -10,20 +10,26 @@ namespace JerryStore.Controllers
     public class ServicesController : Controller
     {
         // GET: Students/
-        public ActionResult Index(int?ID)
+        public ActionResult Index(int? ID)
         {
-             
-            if (ID == 1)
-         {
-                return View(new Service { ID = 1, Package = "Starter", Task = "5", Assistant = "Random Assistant", Price = 19.95m });
+
+            using (Models.CustomersEntities entities = new Models.CustomersEntities())
+            {
+                var package = entities.Packages.Single(x => x.ID == ID);
+                ServiceModel model = new ServiceModel();
+                model.id = package.ID;
+                model.Name = package.Name;
+                model.Price = package.Price;
+                return View(model);
+            }
+            
+          //      return View(new Service { ID = 1, Package = "Starter", Task = "5", Assistant = "Random Assistant", Price = 19.95m });
 
                 //services.Add(new Service { ID = 2, Package = "Professional", Task = "25", Assistant = "Dedicated Assistant", Price = 19.95 });
 
                // services.Add(new Service { ID = 3, Package = "Executive", Task = "50", Assistant = "Random Assistant", Price = 179.95 });//
 
-            }
-
-            return HttpNotFound ("No service found");
+            
         }
         // POST: Students/Edit/{id}
         [HttpPost]
---
+++ JerryStore/Models/CheckoutModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace JerryStore.Models
+{
+    public class CheckoutModel
+    {
+        
+        
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+
+            [Required]
+            [EmailAddress]
+            public string EmailAddress { get; set; }
+
+            [Required]
+            [Phone]
+            public string PhoneNumber { get; set; }
+
+            [Required]
+            [CreditCard]
+            public string CreditCardNumber { get; set; }
+
+            [Required]
+            public string CreditCardName { get; set; }
+
+            [Required]
+            [RegularExpression("/^[0-9]{3,4}$/", ErrorMessage = "Your CVV Is Invalid!")]
+            public string CreditCardVerificationValue { get; set; }
+
+            [Required]
+            [Range(01, 12)]
+            public int CreditCardExpirationMonth { get; set; }
+
+            [Required]
+            [Range(2000, 3000)]
+            public int CreditCardExpirationYear { get; set; }
+        }
+    }
