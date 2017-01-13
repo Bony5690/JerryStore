1ff92c1225b0e8c7c750e9fe593a9d025e696a21
Jerry bony <jerrybony@yahoo.com>
2017-01-13 14:11:37 -0600

    update

--- Jerry'sStore/dbo/Tables/CustomerPackages.sql
+++ Jerry'sStore/dbo/Tables/CustomerPackages.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[CustomerPackages] (
     [CustomerID]   INT NOT NULL,
     [PackageID]    INT NOT NULL,
+    [PurchaseDate] DATETIME NULL, 
     CONSTRAINT PK_CustomerPackages PRIMARY KEY CLUSTERED ([CustomerID] ASC, [PackageID] ASC),
     CONSTRAINT FK_CustomerPackages_Customers FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([ID]),
     CONSTRAINT FK_CustomerPackages_Packages FOREIGN KEY ([PackageID]) REFERENCES [dbo].[Packages] ([ID]), 
--- Jerry'sStore/dbo/Tables/Customers.sql
+++ Jerry'sStore/dbo/Tables/Customers.sql
@@ -2,7 +2,7 @@
     [ID]        INT            IDENTITY (1, 1) NOT NULL,
     [FirstName] NVARCHAR (100) NULL,
     [LastName]  NVARCHAR (100) NULL,
-    [EmailAddress:] NCHAR(100) NULL, 
+    [EmailAddress] NCHAR(100) NULL, 
     CONSTRAINT PK_Customers PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
